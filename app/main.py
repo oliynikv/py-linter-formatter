eb45efa556d3bda91c77f51e820972d4766001ba
vitaliyoliynik <oliynikvs@gmail.com>
2026-03-13 23:03:30 +0100

    update main.py

--- app/main.py
+++ app/main.py
@@ -6,16 +6,11 @@ def format_linter_error(error: dict) -> dict:
                 "name": error["code"],
                 "source": "flake8",
             }
-
-
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {"errors": [format_linter_error(error) for error in errors],
             "path": file_path,
             "status": "failed" if errors else "passed",
             }
-
-
-
 def format_linter_report(linter_report: dict) -> list:
     return [
         format_single_linter_file(file_path, errors)
